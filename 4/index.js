af8d6dafc8f692b6fcf3b6d932d7f8fe5b777904
Sardor <sardoribodov07@gmail.com>
2023-07-11 17:20:01 +0500

    Switch-case

---
+++ 4/index.js
@@ -0,0 +1,20 @@
+// switch-case
+// let light = "24324234";
+// switch(light) {
+// 	case "red": console.log("Stop"); break;
+// 	case "yellow": console.log("Be careful"); break;
+// 	case "green": console.log("Ride"); break;
+// 	default: console.log("Error 404");
+// }
+// if(light === "red") console.log("Stop");
+// else if(light === "yellow") console.log("Be careful");
+// else if(light === "green") console.log("Ride");
+// else console.log("Error 404");
+
+// let son = -1;
+// if(son > 0) console.log(++son);
+// else console.log(son);
+// if(son > 0) {
+// 	son++
+// }
+// console.log(son);
