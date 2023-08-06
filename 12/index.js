47670644720e2288f2c69cbb989506dabea3a5f3
Sardor <sardoribodov07@gmail.com>
2023-08-06 16:47:53 +0500

    Array 2

--- 12/index.js
+++ 12/index.js
@@ -150,3 +150,16 @@
 //   return value + current;
 // }, 0);
 // console.log(sum);
+
+// function tidyNumber(n) {
+//   let isTidy = true;
+//   const array = `${n}`.split("");
+//   const newArray = array.map((v) => +v);
+//   let sortedArray = [...newArray];
+//   sortedArray = sortedArray.sort((a, b) => a - b);
+//   for (let i = 0; i < newArray.length; i++) {
+//     if (newArray[i] !== sortedArray[i]) isTidy = false;
+//   }
+//   return isTidy;
+// }
+// console.log(tidyNumber(102));
