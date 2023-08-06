a04395c2297f0aaced08114d200ea485dbaf829e
Sardor <sardoribodov07@gmail.com>
2023-08-06 16:28:02 +0500

    Array 2

--- 12/index.js
+++ 12/index.js
@@ -132,3 +132,21 @@
 //   return true;
 // });
 // console.log(result);
+
+//? fill(to'ldirmoqchi bo'lgan element, boshlang'ich index, tugash index(bu kirmaydi)) => to'ldirish
+// const numbers = [1, 2, 3, 4, 5];
+// numbers.fill("webbrain", 1, 4); // [1, "webbrain", "webbrain", "webbrain", 5]
+// numbers.fill("webbrain", 4, 8)
+// console.log(numbers);
+
+//? copyWithin(o'zgartirilayotgan qiymat index, qo'yilayotgan elementni boshlang'ich index, qo'yilayotgan elementni oxirgi index(bu kirmaydi))
+// const numbers = [1, 2, 3, 4, 5]; // [4, 5, 3, 4, 5]
+// numbers.copyWithin(0, 3, 5);
+// console.log(numbers);
+
+//? reduce((ikkinchi, hozirgi) => {})
+// const numbers = [1, 2, 3];
+// let sum = numbers.reduce((value, current) => {
+//   return value + current;
+// }, 0);
+// console.log(sum);
