cd5b72e356873a36fafd7d7bef05ffa01175883f
Sardor <sardoribodov07@gmail.com>
2023-08-22 16:56:17 +0500

    setTimeout | setInterval

---
+++ 19/index.js
@@ -0,0 +1,51 @@
+//! Sinxron kod
+// console.log(1);
+// console.log(2);
+// console.log(3);
+
+//! Asinxron kod
+//? asinxron kod
+//? ...
+//? ...
+//? ...
+//? ...
+
+//! setTimeout(() => {}, time)
+// setTimeout(() => {
+//   console.log(1);
+// }, 5000);
+// console.log("webbrain");
+
+//! savollar
+// console.log(1);
+// setTimeout(() => console.log(2), 0);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+//! event loop
+
+//! setInterval
+// let i = 0;
+// setInterval(() => {
+//   i++;
+//   console.log(i);
+// }, 1000);
+
+//! soat
+// let h = 0;
+// let m = 0;
+// let s = 0;
+// setInterval(() => {
+//   s++;
+//   if (s === 60) {
+//     m++;
+//     s = 0;
+//   }
+//   if (m === 60) {
+//     h++;
+//     m = 0;
+//   }
+//   console.log(`${h}:${m}:${s}`);
+// }, 1000);
